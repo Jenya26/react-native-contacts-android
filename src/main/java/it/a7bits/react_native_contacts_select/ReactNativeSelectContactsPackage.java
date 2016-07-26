e57484be079d7d97a53beef2f8098de42cab8b97
Eugene <Jenya266@gmail.com>
2016-07-26 15:42:11 +0600

    react-native-contacts has been created

---
+++ src/main/java/it/a7bits/react_native_contacts_select/ReactNativeSelectContactsPackage.java
@@ -0,0 +1,30 @@
+package it.a7bits.react_native_contacts_select;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.*;
+
+public class ReactNativeSelectContactsPackage implements ReactPackage{
+
+    private ReactNativeSelectContacts selectContacts;
+
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        selectContacts = new ReactNativeSelectContacts(reactContext);
+        return Arrays.<NativeModule>asList(selectContacts);
+    }
+
+    @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+}
